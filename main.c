ad503a683180cb3e41d75e1874f245f7aa14b8e6
Mateus Lima <mateuslimaw@gmail.com>
2026-04-30 17:30:48 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,78 +1,104 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define quantum 2
-struct Tarefa { //TAREFAS
+
+#define QUANTUM 2
+#define OCUPADO 1
+#define LIVRE 0
+
+//TAREFA
+typedef struct Tarefa {
+    int id;
     int tempo_execucao;
-};
-struct No {                 // INICIO FUNÇÕES FILA
-    struct Tarefa *dado;
-    struct No *proximo;
-};
-struct Fila {
-    struct No *frente;
-    struct No *tras;
-};
-void enfileirar(struct Fila *f, struct Tarefa *nova_tarefa) {
-    struct No *novo_no = malloc(sizeof(struct No));
-    novo_no->dado = nova_tarefa;
-    novo_no->proximo = NULL;
+} Tarefa;
+//PROCESSADOR
+typedef struct CPU {
+    int size;
+    int tempo_desligado;
+    int ocupado;
+}CPU;
 
-    if (f->tras == NULL) {
-        f->frente = f->tras = novo_no;
-        return;
-    }
-    f->tras->proximo = novo_no;
-    f->tras = novo_no;
-}
-struct Tarefa desenfileirar(struct Fila *f) {
-    if (f->frente == NULL) {
-        printf("Fila vazia!\n");
-        exit(1);
-    }
-    struct No *temp = f->frente;
-    struct Tarefa *t = temp->dado;
-    f->frente = f->frente->proximo;
+//FILA
+typedef struct No {
+    Tarefa dado;
+    struct No *prox;
+} No;
+typedef struct {
+    No *inicio;
+    No *fim;
+} Fila;
+void execucao_passo_a_passo();
+void execucao_completa();
+void inserir(Fila *f, Tarefa t);
+Tarefa remover(Fila *f);
+void executar_tarefa(Tarefa *t);
+int vazia(Fila *f);
+void inicializar_fila(Fila *f);
 
-    if (f->frente == NULL) f->tras = NULL;
-    free(temp);
-    return *t;
-}
-int empty(struct Fila *f) {
-    if (f->frente == NULL)
-        return 1;
-    return 0;                           //FIM DAS FUNÇÕES DA FILA
-}
-struct Tarefa inicializar_tarefa() {    //INICIALIZA TAREFA
-    srand(time(NULL));
-    struct Tarefa tarefa;
-    tarefa.tempo_execucao = rand()%10 + 1; //ATRIBUI VALOR ALEATÓRIO PARA O TEMPO DE EXECUÇÃO
-    return tarefa;
-}
-void executar_tarefa (struct Tarefa *tarefa) {      //SIMULA EXECUÇÃO NO PROCESSADOR
-    time_t inicio, agora;
-    int tempo_execucao = 0;
-    while (tempo_execucao < quantum && tarefa->tempo_execucao > 0) { //ENQUANTO EXECUTA NO TEMPO DO QUANTUM
-        time(&inicio);                                              //E ENQUANTO A TAREFA AINDA PODE EXECUTAR
-        //REGISTA HORARIO DE INICIO
-        do {
-            time(&agora); //ATUALIZA O HORARIO ATUAL ATE TER A DIFERENÇA DE 1 SEGUNDO
-        }while(difftime(agora, inicio) < 1.0);
-        tempo_execucao += 1; //REGISTRA TEMPO DE EXECUÇÃO
-        tarefa->tempo_execucao -= 1; //REGISTRA EXECUÇÃO DA TAREFA
-    }
-}
 int main(void) {
-    struct Tarefa tarefaA = inicializar_tarefa(); //TAREFAS
-    struct Tarefa tarefaB = inicializar_tarefa();
-    struct Tarefa tarefaC = inicializar_tarefa();
-    struct Fila f;
-    enfileirar(&f, &tarefaA); //FILA
-    enfileirar(&f, &tarefaB);
-    enfileirar(&f, &tarefaC);
-    while (!empty(&f)) { //ENQUANTO A FILA NÃO ESTA VAZIA
-        executar_tarefa(&tarefaA);
+    Fila f_tarefas;
+    inicializar_fila(&f_tarefas);
+    Tarefa t1 = {1, 6};
+    Tarefa t2 = {2, 3};
+
+    inserir(&f_tarefas, t1);
+    inserir(&f_tarefas, t2);
+
+    while (!vazia(&f_tarefas)) {
+        Tarefa atual = remover(&f_tarefas);
+
+        executar_tarefa(&atual);
+
+        if (atual.tempo_execucao > 0) {
+            inserir(&f_tarefas, atual);
+        }
     }
 
     return 0;
+}
+
+
+//FUNÇÕES FILA
+void inicializar_fila(Fila *f) {
+    f->inicio = NULL;
+    f->fim = NULL;
+}
+int vazia(Fila *f) {
+    return f->inicio == NULL;
+}
+void inserir(Fila *f, Tarefa t) {
+    No *novo = (No *)malloc(sizeof(No));
+    if (novo == NULL) return;
+    novo->dado = t;
+    novo->prox = NULL;
+    if (vazia(f)) {
+        f->inicio = novo;
+        f->fim = novo;
+    } else {
+        f->fim->prox = novo;
+        f->fim = novo;
+    }
+}
+Tarefa remover(Fila *f) {
+    No *temp = f->inicio;
+    Tarefa t = temp->dado;
+    f->inicio = f->inicio->prox;
+    if (f->inicio == NULL) {
+        f->fim = NULL;
+    }
+    free(temp);
+    return t;
+}
+
+//ROUND-ROBIN - NÃO É PARA USAR -
+void executar_tarefa(Tarefa *t) {
+    int tempo_gasto = 0;
+
+    while (tempo_gasto < QUANTUM && t->tempo_execucao > 0) {
+        clock_t goal = clock() + CLOCKS_PER_SEC;
+        while (clock() < goal);
+
+        t->tempo_execucao--;
+        tempo_gasto++;
+    }
 }
